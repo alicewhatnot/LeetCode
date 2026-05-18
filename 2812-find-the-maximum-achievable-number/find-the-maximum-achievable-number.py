80d5c4527031dd49d9fe908d36c71221f9ccf930
Alice <alicegillbanks@gmail.com>
2026-05-18 12:15:05 +0100

    Time: 0 ms (100.00%) | Memory: 19.3 MB (45.10%) - LeetSync

--- 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.py
+++ 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.py
@@ -1,8 +1,4 @@
 class Solution:
     def theMaximumAchievableX(self, num: int, t: int) -> int:
-        
-        # t operations
-        # make num bigger 
-        # or x smaller
 
         return num + 2*t
